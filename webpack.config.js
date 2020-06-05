9aa89349a639cb6c95aa3dab127ca6c8028e598c
aei <aeifrontend@gmail.com>
2020-06-05 23:26:19 +0500

    Update dependencies and small refactoring
    fix alerts
    add error catch when font is not loaded
    refactoring
    update dependencies
    delete figma.d.ts

--- webpack.config.js
+++ webpack.config.js
@@ -1,54 +1,57 @@
-const HtmlWebpackInlineSourcePlugin = require('html-webpack-inline-source-plugin')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const path = require('path')
-const TerserPlugin = require('terser-webpack-plugin');
-const CopyPlugin = require('copy-webpack-plugin');
-
+const HtmlWebpackInlineSourcePlugin = require("html-webpack-inline-source-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = (env, argv) => ({
-  mode: argv.mode === 'production' ? 'production' : 'development',
+  mode: argv.mode === "production" ? "production" : "development",
 
   // This is necessary because Figma's 'eval' works differently than normal eval
-  devtool: argv.mode === 'production' ? false : 'inline-source-map',
+  devtool: argv.mode === "production" ? false : "inline-source-map",
 
   entry: {
     //ui: './src/ui.ts', // The entry point for your UI code
-    code: './code.ts', // The entry point for your plugin code
+    code: "./code.ts", // The entry point for your plugin code
   },
 
   module: {
     rules: [
       // Converts TypeScript code to JavaScript
-      { test: /\.tsx?$/, use: 'ts-loader', exclude: /node_modules/ },
+      { test: /\.tsx?$/, use: "ts-loader", exclude: /node_modules/ },
 
       // Enables including CSS by doing "import './file.css'" in your TypeScript code
-      { test: /\.css$/, loader: [{ loader: 'style-loader' }, { loader: 'css-loader' }] },
+      {
+        test: /\.css$/,
+        loader: [{ loader: "style-loader" }, { loader: "css-loader" }],
+      },
 
       // Allows you to use "<%= require('./file.svg') %>" in your HTML code to get a data URI
-      { test: /\.(png|jpg|gif|webp|svg)$/, loader: [{ loader: 'url-loader' }] },
+      { test: /\.(png|jpg|gif|webp|svg)$/, loader: [{ loader: "url-loader" }] },
+    ],
+  },
+  devtool: false,
+  plugins: [],
+  // Webpack tries these extensions for you if you omit the extension like "import './file'"
+  resolve: { extensions: [".tsx", ".ts", ".jsx", ".js"] },
+
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          output: {
+            comments: false,
+          },
+        },
+        extractComments: false,
+      }),
     ],
   },
 
-  plugins: [
-    new CopyPlugin([
-      { from: './manifest.json', to: '.' },
-    ]),
-  ],
-  // Webpack tries these extensions for you if you omit the extension like "import './file'"
-  resolve: { extensions: ['.tsx', '.ts', '.jsx', '.js'] },
-
-    optimization: {
-      minimize: true,
-      minimizer: [new TerserPlugin({
-        extractComments: false,
-      }),]
-    },
-  
   output: {
-    filename: '[name].js',
-    path: path.resolve(__dirname, 'dist'), // Compile into a folder called "dist"
+    filename: "[name].js",
+    path: path.resolve(__dirname, "dist"), // Compile into a folder called "dist"
   },
 
   // Tells Webpack to generate "ui.html" and to inline "ui.ts" into it
- 
-})
+});
