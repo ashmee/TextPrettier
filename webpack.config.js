8894bf729e6ae2285e06c6f7a19816bb00c22f8a
aei <ashmee90@gmail.com>
2021-02-28 22:26:30 +0500

    update package versions and started to add config menu

--- webpack.config.js
+++ webpack.config.js
@@ -1,57 +1,65 @@
-const HtmlWebpackInlineSourcePlugin = require("html-webpack-inline-source-plugin");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const path = require("path");
-const TerserPlugin = require("terser-webpack-plugin");
+//const HtmlWebpackInlineSourcePlugin = require('html-webpack-inline-source-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path')
+const TerserPlugin = require('terser-webpack-plugin')
 
 module.exports = (env, argv) => ({
-  mode: argv.mode === "production" ? "production" : "development",
+    mode: argv.mode === 'production' ? 'production' : 'development',
 
-  // This is necessary because Figma's 'eval' works differently than normal eval
-  devtool: argv.mode === "production" ? false : "inline-source-map",
+    // This is necessary because Figma's 'eval' works differently than normal eval
+    devtool: argv.mode === 'production' ? false : 'inline-source-map',
 
-  entry: {
-    //ui: './src/ui.ts', // The entry point for your UI code
-    code: "./code.ts", // The entry point for your plugin code
-  },
+    entry: {
+        //ui: './src/ui.ts', // The entry point for your UI code
+        code: './src/code.ts', // The entry point for your plugin code
+    },
 
-  module: {
-    rules: [
-      // Converts TypeScript code to JavaScript
-      { test: /\.tsx?$/, use: "ts-loader", exclude: /node_modules/ },
+    module: {
+        rules: [
+            // Converts TypeScript code to JavaScript
+            { test: /\.tsx?$/, use: 'ts-loader', exclude: /node_modules/ },
 
-      // Enables including CSS by doing "import './file.css'" in your TypeScript code
-      {
-        test: /\.css$/,
-        loader: [{ loader: "style-loader" }, { loader: "css-loader" }],
-      },
+            // Enables including CSS by doing "import './file.css'" in your TypeScript code
 
-      // Allows you to use "<%= require('./file.svg') %>" in your HTML code to get a data URI
-      { test: /\.(png|jpg|gif|webp|svg)$/, loader: [{ loader: "url-loader" }] },
+            { test: /\.(png|jpg|gif|webp|svg)$/, loader: 'url-loader' },
+        ],
+    },
+    resolve: { extensions: ['.tsx', '.ts', '.jsx', '.js'] },
+
+    optimization: {
+        minimize: true,
+        minimizer: [
+            new TerserPlugin({
+                terserOptions: {
+                    output: {
+                        comments: false,
+                    },
+                },
+                extractComments: false,
+            }),
+        ],
+    },
+
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname, 'dist'), // Compile into a folder called "dist"
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/ui.html',
+            filename: 'ui.html',
+            minify: {
+                collapseWhitespace: true,
+                keepClosingSlash: true,
+                removeComments: true,
+                removeRedundantAttributes: true,
+                removeScriptTypeAttributes: true,
+                removeStyleLinkTypeAttributes: true,
+                useShortDoctype: true,
+                minifyCSS: true,
+                minifyJS: true,
+            },
+        }),
     ],
-  },
-  devtool: false,
-  plugins: [],
-  // Webpack tries these extensions for you if you omit the extension like "import './file'"
-  resolve: { extensions: [".tsx", ".ts", ".jsx", ".js"] },
-
-  optimization: {
-    minimize: true,
-    minimizer: [
-      new TerserPlugin({
-        terserOptions: {
-          output: {
-            comments: false,
-          },
-        },
-        extractComments: false,
-      }),
-    ],
-  },
-
-  output: {
-    filename: "[name].js",
-    path: path.resolve(__dirname, "dist"), // Compile into a folder called "dist"
-  },
-
-  // Tells Webpack to generate "ui.html" and to inline "ui.ts" into it
-});
+})
